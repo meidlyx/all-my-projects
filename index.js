504f54c4ce61e4625809b8b32bc6d0c3a497e6b5
Meidlyx <120543175+meidlyx@users.noreply.github.com>
2024-04-28 00:14:19 +0500

    Update index.js
    new update js

--- index.js
+++ index.js
@@ -3,39 +3,23 @@ function showPopup(contentId) {
     var popupContent1 = document.getElementById('popupContent1');
     var popupContent2 = document.getElementById('popupContent2');
     var popupContent3 = document.getElementById('popupContent3');
-    var popupContent4 = document.getElementById('popupContent4');
-    var popupContent5 = document.getElementById('popupContent5');
 
     if (contentId === 1) {
         popupContent1.style.display = 'block';
         popupContent2.style.display = 'none';
         popupContent3.style.display = 'none';
-        popupContent4.style.display = 'none';
-        popupContent5.style.display = 'none';
     } else if (contentId === 2) {
         popupContent1.style.display = 'none';
         popupContent2.style.display = 'block';
         popupContent3.style.display = 'none';
-        popupContent4.style.display = 'none';
-        popupContent5.style.display = 'none';
     } else if (contentId === 3) {
         popupContent1.style.display = 'none';
         popupContent2.style.display = 'none';
         popupContent3.style.display = 'block';
-        popupContent4.style.display = 'none';
-        popupContent5.style.display = 'none';
     } else if (contentId === 4) {
         popupContent1.style.display = 'none';
         popupContent2.style.display = 'none';
         popupContent3.style.display = 'none';
-        popupContent4.style.display = 'block';
-        popupContent5.style.display = 'none';
-    } else if (contentId === 5) {
-        popupContent1.style.display = 'none';
-        popupContent2.style.display = 'none';
-        popupContent3.style.display = 'none';
-        popupContent4.style.display = 'none';
-        popupContent5.style.display = 'block';
     }
 
     popup.classList.add('active');
@@ -46,14 +30,10 @@ function closePopup() {
     var popupContent1 = document.getElementById('popupContent1');
     var popupContent2 = document.getElementById('popupContent2');
     var popupContent3 = document.getElementById('popupContent3');
-    var popupContent4 = document.getElementById('popupContent4');
-    var popupContent5 = document.getElementById('popupContent5');
 
     popupContent1.style.display = 'none';
     popupContent2.style.display = 'none';
     popupContent3.style.display = 'none';
-    popupContent4.style.display = 'none';
-    popupContent5.style.display = 'none';
 
     popup.classList.remove('active');
 }
@@ -69,11 +49,34 @@ document.getElementById('showPopupButton2').addEventListener('click', function()
 document.getElementById('showPopupButton3').addEventListener('click', function() {
     showPopup(3);
 });
-document.getElementById('showPopupButton4').addEventListener('click', function() {
-    showPopup(4);
-});
-document.getElementById('showPopupButton5').addEventListener('click', function() {
-    showPopup(5);
-});
 
 document.getElementById('closePopupButton').addEventListener('click', closePopup);
+
+function toggleAside() {
+    var aside = document.getElementById("myAside");
+    var computedStyle = window.getComputedStyle(aside);
+    var left = computedStyle.getPropertyValue("left");
+    var closeAsideButton = document.getElementById("closeAsideButton");
+
+    if (left === "-495px" || left === "") {
+        aside.style.left = "0";
+        aside.classList.add("open"); 
+        closeAsideButton.style.display = 'block';
+    } else {
+        aside.style.left = "-495px";
+        aside.classList.remove("open"); 
+        closeAsideButton.style.display = 'none';
+    }
+}
+
+
+function closeAside() {
+    var aside = document.getElementById("myAside");
+    aside.style.left = "-495px";
+    aside.classList.remove("open"); 
+}
+
+
+document.getElementById("showPopupButton1").addEventListener("click", toggleAside);
+document.getElementById("showPopupButton2").addEventListener("click", toggleAside);
+document.getElementById("showPopupButton3").addEventListener("click", toggleAside);
