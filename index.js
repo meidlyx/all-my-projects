e2d077cc22c08b0425b436433ed2ed9bada676a1
Meidlyx <120543175+meidlyx@users.noreply.github.com>
2024-05-22 17:03:02 +0500

    Update index.js
    update 5.0.0

--- index.js
+++ index.js
@@ -4,27 +4,38 @@ function showPopup(contentId) {
     var popupContent2 = document.getElementById('popupContent2');
     var popupContent3 = document.getElementById('popupContent3');
     var popupContent4 = document.getElementById('popupContent4');
+    var popupContent5 = document.getElementById('popupContent5');
 
     if (contentId === 1) {
         popupContent1.style.display = 'block';
         popupContent2.style.display = 'none';
         popupContent3.style.display = 'none';
         popupContent4.style.display = 'none';
+        popupContent5.style.display = 'none';
     } else if (contentId === 2) {
         popupContent1.style.display = 'none';
         popupContent2.style.display = 'block';
         popupContent3.style.display = 'none';
         popupContent4.style.display = 'none';
+        popupContent5.style.display = 'none';
     } else if (contentId === 3) {
         popupContent1.style.display = 'none';
         popupContent2.style.display = 'none';
         popupContent3.style.display = 'block';
         popupContent4.style.display = 'none';
+        popupContent5.style.display = 'none';
     } else if (contentId === 4) {
         popupContent1.style.display = 'none';
         popupContent2.style.display = 'none';
         popupContent3.style.display = 'none';
         popupContent4.style.display = 'block';
+        popupContent5.style.display = 'none';
+    } else if (contentId === 5) {
+        popupContent1.style.display = 'none';
+        popupContent2.style.display = 'none';
+        popupContent3.style.display = 'none';
+        popupContent4.style.display = 'none';
+        popupContent5.style.display = 'block';
     }
 
     popup.classList.add('active');
@@ -37,11 +48,13 @@ function closePopup() {
     var popupContent2 = document.getElementById('popupContent2');
     var popupContent3 = document.getElementById('popupContent3');
     var popupContent4 = document.getElementById('popupContent4');
+    var popupContent5 = document.getElementById('popupContent5');
 
     popupContent1.style.display = 'none';
     popupContent2.style.display = 'none';
     popupContent3.style.display = 'none';
     popupContent4.style.display = 'none';
+    popupContent5.style.display = 'none';
 
     popup.classList.remove('active');
 }
@@ -60,6 +73,10 @@ document.getElementById('showPopupButton3').addEventListener('click', function()
 
 document.getElementById('showPopupButton4').addEventListener('click', function() {
     showPopup(4);
+});
+
+document.getElementById('showPopupButton5').addEventListener('click', function() {
+    showPopup(5);
 });
 
 document.getElementById('closePopupButton').addEventListener('click', closePopup);
